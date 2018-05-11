42c15feb75131d50333d5c32fe9dd1c1fe52775e
Chris <gateway.kranz@gmail.com>
2018-05-11 14:32:49 +0200

    Change location via modal completed

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const ui = new UI();
 
 // Get weather on DOMload
 document.addEventListener('DOMContentLoaded', getWeather);
+// Change location Event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+
+    weather.changeLocation(city, state);
+    //Get and Display Weather for new Location
+    getWeather();
+    //Close Modal (jQuery)
+    $('#locModal').modal('hide');
+
+});
 
 function getWeather() {
     weather.getWeather()
