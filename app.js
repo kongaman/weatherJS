c9d04c996716ef41102c347199ed061738d56936
Chris <gateway.kranz@gmail.com>
2018-05-11 14:48:35 +0200

    completed

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Init calsses
-const weather = new Weather('Boston', 'MA');
 const ui = new UI();
+const storage = new Storage();
+//Get stored Location data
+const weatherLocation = storage.getLocationData();
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 
 // Get weather on DOMload
 document.addEventListener('DOMContentLoaded', getWeather);
@@ -8,13 +11,14 @@ document.addEventListener('DOMContentLoaded', getWeather);
 document.getElementById('w-change-btn').addEventListener('click', (e) => {
     const city = document.getElementById('city').value;
     const state = document.getElementById('state').value;
-
+    // Change Location via Modal
     weather.changeLocation(city, state);
+    // Set Location in localStorage
+    storage.setLocationData(city, state);
     //Get and Display Weather for new Location
     getWeather();
     //Close Modal (jQuery)
     $('#locModal').modal('hide');
-
 });
 
 function getWeather() {
